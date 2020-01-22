42105c04cb31aa3b2a29561ea5f4fa63f8964bcf
Carlos <miklo.concepcion@gmail.com>
2020-01-22 00:16:19 -0600

    setting up seeds file path in knexfile.js. then going to run my seed and then run cleanup.js 000?

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,9 @@ module.exports = {
     // generates migration files in a data/migrations/ folder
     migrations: {
       directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   }
 
